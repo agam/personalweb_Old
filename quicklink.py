10612f69ef64a0b98a95383d969c3ca3e1013bf2
Agam <agam.brahma@gmail.com>
2011-12-16 23:01:58 -0800

    Added quicklink addition by email

--- quicklink.py
+++ quicklink.py
@@ -3,6 +3,7 @@ import os
 
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from models import QuickLink
@@ -29,12 +30,31 @@ class GetQuickLink(webapp.RequestHandler):
 
 class ShowAllQuickLinks(webapp.RequestHandler):
     def get(self):
+        #TODO(agam): Implement this
         pass
+
+class  AddQuickLink(InboundMailHandler):
+    def receive(self, mail_message):
+        if mail_message.sender != 'Agam Brahma <agam.brahma@gmail.com>':
+            logging.error('Received email from an email address other than my own ?!')
+            return
+
+        text_bodies = list(mail_message.bodies('text/plain'))
+        if len(text_bodies) > 1:
+            logging.info("Weird -- received multi-part message!")
+        body = text_bodies[0]
+        logging.info("%s: %s" % ('Message bodies', ''.join('msg %d: %s; ' % x for x in enumerate(text_bodies))))
+
+        logging.debug("Received message from : " + mail_message.sender + ", to : " + mail_message.to + ", with subject: " + mail_message.subject + ", messsage is : " + body[1].decode())
+
+        link = QuickLink(shortlink=mail_message.subject, longlink=body[1].decode())
+        link.put()
 
 application = webapp.WSGIApplication(
         [('/quicklink/([^/]+)', GetQuickLink),
-         ('/.*$', ShowAllQuickLinks),
-         ], debug=True)
+            ('/.*$', ShowAllQuickLinks),
+            AddQuickLink.mapping(),
+            ], debug=True)
 
 def main():
     run_wsgi_app(application)
